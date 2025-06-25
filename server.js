ab1a139ee1bbcd6294a631bfc8a103cb75b9a7b9
Philip Njoroge <131545679+PhilTech-pixel@users.noreply.github.com>
2025-06-25 22:52:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+
 app.use(express.json());
 app.use("/api/contacts", contactRouter);
 app.use("/api/users", userRouter);
